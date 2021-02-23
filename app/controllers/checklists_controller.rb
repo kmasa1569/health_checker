dbc6690fb7e36b0369e72e444e8cfff3f54e1c76
masaya kawamura <m.kawamura@masayanoMacBook-Pro.local>
2021-02-24 07:16:59 +0900

    checklist index,new,create,showの実装

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -4,12 +4,29 @@ class ChecklistsController < ApplicationController
   end
 
   def show
+    @checklist = Checklist.find(params[:id])
   end
 
   def new
     @checklist = Checklist.new
   end
 
+  def create
+    checklist = Checklist.new(checklist_params)
+    if checklist.save
+      redirect_to checklists_url, notice: '本日の値を入力しました。'
+    else
+      flash.now[:alert] = '何か入力に間違いがあるようです。'
+      render :new
+    end
+  end
+
   def edit
   end
+
+  private
+   def checklist_params
+     params.require(:checklist).permit(:date, :bt, :hr, :sbp, :dbp, :wt)
+   end
+   
 end
