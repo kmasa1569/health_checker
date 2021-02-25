df6ae47b98d777d6b568f474b4d00a821bf36332
masaya kawamura <m.kawamura@masayanoMacBook-Pro.local>
2021-02-25 21:09:39 +0900

    postgresqlの導入

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_23_111051) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "checklists", force: :cascade do |t|
     t.date "date"
     t.float "bt"
