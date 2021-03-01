d4ec8970b592effdbc31f365dcd3c59739e93857
kmasa1569 <kmasa1569@gmail.com>
2021-03-01 20:16:43 +0900

    medical staffモデルの作成

---
+++ app/models/medical_staff.rb
@@ -0,0 +1,2 @@
+class MedicalStaff < ApplicationRecord
+end
---
+++ spec/factories/medical_staffs.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :medical_staff do
+    name { "MyString" }
+    email { "MyString" }
+    password_digest { "MyString" }
+  end
+end
