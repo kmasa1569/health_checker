3f8fdc2f03a821474405ac82a55b94482f01461f
kmasa1569 <kmasa1569@gmail.com>
2021-03-06 23:58:06 +0900

    単一テーブル継承を用いてPatientとStaffを作成

--- app/models/medical_staff.rb
+++
@@ -1,2 +0,0 @@
-class MedicalStaff < ApplicationRecord
-end
--- spec/factories/medical_staffs.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :medical_staff do
-    name { "MyString" }
-    email { "MyString" }
-    password_digest { "MyString" }
-  end
-end
